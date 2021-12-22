9ed308275da7526e3cb60124730a9bd8a19e1d57
Franco Pereyra <francopereyra99@gmail.com>
2021-12-22 16:30:57 +0100

    Liskov Substitution Correcto ejemplo Closes #6

---
+++ Principios Solid/LiskovSubstitution/Bien/Audi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.LiskovSubstitution.Bien
+{
+    public class Audi : Coche
+    {
+        
+        public override int numAsientos()
+        {
+            return 25000;
+        }
+        
+        public override int precioMedioCoche()
+        {
+            return 5;
+        }
+    }
+}
---
+++ Principios Solid/LiskovSubstitution/Bien/Principal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.LiskovSubstitution.Bien
+{
+    public class Principal
+    {
+        public static void Main(string[] args)
+        {
+            Coche[] arrayCoches =
+            {
+                new Renault(),
+                new Audi(),
+                new Mercedes()
+            };
+            imprimirPrecioMedioCoche(arrayCoches);
+
+        }
+        public static void imprimirPrecioMedioCoche(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.precioMedioCoche());
+            }
+        }
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach(Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.numAsientos());
+            }
+        }
+    }
+}
