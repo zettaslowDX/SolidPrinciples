567071740a046e209699d7382a759c2bbd8aff92
Franco Pereyra <francopereyra99@gmail.com>
2021-12-22 17:22:56 +0100

    Interface Segregation Principle Example Closes #8

---
+++ Principios Solid/InterfaceSegregation/Mal/IAve.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.InterfaceSegregation.Mal
+{
+    interface IAve
+    {
+        void volar();
+        void comer();
+        void nadar();
+    }
+
+    public class Loro:IAve
+    {
+
+    
+    public  void volar()
+    {
+        //...
+    }
+    
+    public void comer()
+    {
+        //...
+    }
+
+    
+    public void nadar()
+    {
+        //...
+    }
+}
+
+public class Pinguino: IAve
+{
+
+    
+    public void volar()
+{
+    //...
+}
+
+
+    public void comer()
+{
+    //...
+}
+
+
+    public void nadar()
+{
+    //...
+}
+}
+}
