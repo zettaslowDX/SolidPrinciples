a65732b5c1ceabed487874d33876e3b88ba3fa85
Franco Pereyra <francopereyra99@gmail.com>
2021-12-22 12:54:53 +0100

    Add Open Close Principal Example closes #3

---
+++ Principios Solid/OpenClose/Bien/Renault.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.OpenClose.Bien
+{
+    class Renault : Coche  {
+        override
+        public int precioMedioCoche()
+        { return 18000; }
+    }
+}
---
+++ Principios Solid/OpenClose/Mal/Coche.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.OpenClose.Mal
+{
+    public class Coche
+    {
+        private string marca;
+        public string Marca { get; set; }
+        public Coche (string marca) { this.marca = marca; }
+        public string getMarcaCoche() { return marca; }
+        
+    }
+
+
+}
+
