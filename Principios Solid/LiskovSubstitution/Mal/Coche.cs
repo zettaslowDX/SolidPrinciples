9d589444a710822b23c629891fb3fdfbd68454b8
Franco Pereyra <francopereyra99@gmail.com>
2021-12-22 16:28:53 +0100

    Liskov substitution ejemplo incorrecto references #6

---
+++ Principios Solid/LiskovSubstitution/Mal/Coche.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.LiskovSubstitution.Mal
+{
+    public class Coche
+    {
+
+    }
+}
---
+++ Principios Solid/LiskovSubstitution/Mal/Principal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_Solid.LiskovSubstitution.Mal
+{
+    public class Principal
+    {
+        public static void Main(string[] args)
+        {
+            Coche[] arrayCoches =
+            {
+                new Renault(),
+                new Audi(),
+                new Mercedes()
+            };
+            imprimirNumAsientos(arrayCoches);
+        }
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                if (coche.GetType() == typeof(Renault))
+                    Console.WriteLine("num asientos 4");
+                if (coche.GetType() == typeof(Audi))
+                    Console.WriteLine("num asientos 5");
+                if (coche.GetType() == typeof(Mercedes))
+                    Console.WriteLine("num asientos 6");
+            }
+        }
+    }
+}
